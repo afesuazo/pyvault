b5bcdc0281ffd2355288819d5d1e993428e8a02a
strider <afesuazo@gmail.com>
2023-04-19 16:34:13 -0600

    Simple site read and optional credential field

--- app/models/site.py
+++ app/models/site.py
@@ -6,23 +6,24 @@ from sqlmodel import Field, SQLModel, Relationship
 # Data only model
 class SiteBase(SQLModel):
     name: str
-    url: Optional[str] = Field(default=None, unique=True)
 
 
 class Site(SiteBase, table=True):
     __tablename__ = "site"
 
     uid: Optional[int] = Field(default=None, primary_key=True, index=True)
+    url: Optional[str] = Field(default=None, unique=True)
 
     # TODO: Add if sites get tied to each user
-    credentials: list["Credential"] = Relationship(back_populates="site", sa_relationship_kwargs={"cascade": "delete", 'lazy': 'selectin'})
+    credentials: list["Credential"] = Relationship(back_populates="site",
+                                                   sa_relationship_kwargs={"cascade": "delete", 'lazy': 'selectin'})
 
 
 # Created to differentiate from Base in docs
 
 
 class SiteCreate(SiteBase):
-    pass
+    url: Optional[str]
 
 
 class SiteUpdate(SiteBase):
@@ -30,4 +31,9 @@ class SiteUpdate(SiteBase):
 
 
 class SiteRead(SiteBase):
-    pass
+    url: Optional[str]
+    uid: int
+
+
+class SiteSimpleRead(SiteBase):
+    uid: int
