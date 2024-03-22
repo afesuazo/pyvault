506d5250e5836e270832548aec49b05aa67ec775
afesuazo <afesuazo@gmail.com>
2024-03-21 20:51:52 -0500

    Add better logging

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
-from fastapi import FastAPI
+import logging
+
+from fastapi import FastAPI, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.base import api_router
 from app.database import PasswordDB
+
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
 
 
 def build_app() -> FastAPI:
     application = FastAPI(title="Password Manager", debug=True, version="1.0")
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
+        allow_origins=origins,
         allow_methods=["*"],
         allow_headers=["*"],
     )
@@ -19,6 +30,14 @@ def build_app() -> FastAPI:
 
 
 app = build_app()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    logging.error(f"{request}: {exc_str}")
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 @app.on_event("startup")
